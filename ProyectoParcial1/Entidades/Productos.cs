e99fbefefe48b8e9cfdce3c67cdf2a45d64da17f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-28 13:14:04 -0400

    Creando la entidad Producto

---
+++ ProyectoParcial1/Entidades/Productos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParcial1.Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int IdProducto { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public float Costo { get; set; }
+        public float ValorEnInventario { get; set; }
+
+        public Productos()
+        {
+            IdProducto = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorEnInventario = 0;
+        }
+    }
+}
