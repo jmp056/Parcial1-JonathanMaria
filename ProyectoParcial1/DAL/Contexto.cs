d3d0025387f53de995daee7c47f26fc3ad47853f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-28 13:27:56 -0400

    Creando la clase Contexto

---
+++ ProyectoParcial1/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoParcial1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParcial1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+        public Contexto() : base("ConStr")
+        {   }
+    }
+}
