6df09576e7e3ae39ef3ed6d7b3702cd1d82a3f96
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-29 00:06:50 -0400

    Terminando la BLL

---
+++ ProyectoParcial1/BLL/ProductosBLL.cs
@@ -0,0 +1,94 @@
+﻿using ProyectoParcial1.DAL;
+using ProyectoParcial1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ProyectoParcial1.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos producto)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Productos.Add(producto) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Productos producto)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var eliminar = db.Productos.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Productos Buscar (int id)
+        {
+            Contexto db = new Contexto();
+            Productos producto = new Productos();
+            try
+            {
+                producto = db.Productos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return producto;
+        }
+    }
+}
