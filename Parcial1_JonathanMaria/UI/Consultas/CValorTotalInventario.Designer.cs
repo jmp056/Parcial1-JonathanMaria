f6aa8f85192be672e901cd98e6b86de594619f99
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-01 01:14:30 -0400

    Terminando el dise;o de la ventana de consulta del inventario

--- Parcial1_JonathanMaria/UI/Consultas/CValorTotalInventario.Designer.cs
+++ Parcial1_JonathanMaria/UI/Consultas/CValorTotalInventario.Designer.cs
@@ -31,12 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CValorTotalInventario));
             this.ActualizarButton = new System.Windows.Forms.Button();
             this.ValorInventarioTextBox = new System.Windows.Forms.TextBox();
+            this.ValorTotal = new System.Windows.Forms.Label();
+            this.DeInventario = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ActualizarButton
             // 
             this.ActualizarButton.Image = global::ProyectoParcial1.Properties.Resources.BotonActualizar;
-            this.ActualizarButton.Location = new System.Drawing.Point(148, 12);
+            this.ActualizarButton.Location = new System.Drawing.Point(147, 21);
             this.ActualizarButton.Name = "ActualizarButton";
             this.ActualizarButton.Size = new System.Drawing.Size(50, 50);
             this.ActualizarButton.TabIndex = 0;
@@ -46,16 +48,38 @@
             // ValorInventarioTextBox
             // 
             this.ValorInventarioTextBox.Enabled = false;
-            this.ValorInventarioTextBox.Location = new System.Drawing.Point(12, 70);
+            this.ValorInventarioTextBox.Location = new System.Drawing.Point(28, 87);
             this.ValorInventarioTextBox.Name = "ValorInventarioTextBox";
-            this.ValorInventarioTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ValorInventarioTextBox.Size = new System.Drawing.Size(169, 20);
             this.ValorInventarioTextBox.TabIndex = 1;
+            // 
+            // ValorTotal
+            // 
+            this.ValorTotal.AutoSize = true;
+            this.ValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ValorTotal.Location = new System.Drawing.Point(25, 25);
+            this.ValorTotal.Name = "ValorTotal";
+            this.ValorTotal.Size = new System.Drawing.Size(79, 16);
+            this.ValorTotal.TabIndex = 2;
+            this.ValorTotal.Text = "Valor total";
+            // 
+            // DeInventario
+            // 
+            this.DeInventario.AutoSize = true;
+            this.DeInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeInventario.Location = new System.Drawing.Point(25, 50);
+            this.DeInventario.Name = "DeInventario";
+            this.DeInventario.Size = new System.Drawing.Size(102, 16);
+            this.DeInventario.TabIndex = 3;
+            this.DeInventario.Text = "de inventario:";
             // 
             // CValorTotalInventario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(210, 111);
+            this.ClientSize = new System.Drawing.Size(225, 139);
+            this.Controls.Add(this.DeInventario);
+            this.Controls.Add(this.ValorTotal);
             this.Controls.Add(this.ValorInventarioTextBox);
             this.Controls.Add(this.ActualizarButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -70,5 +94,7 @@
 
         private System.Windows.Forms.Button ActualizarButton;
         private System.Windows.Forms.TextBox ValorInventarioTextBox;
+        private System.Windows.Forms.Label ValorTotal;
+        private System.Windows.Forms.Label DeInventario;
     }
 }
