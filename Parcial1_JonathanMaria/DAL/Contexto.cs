781d440efa6f4e73d2879b2c57c9a95a1091c939
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-31 20:28:32 -0400

    Agregar ValorTotalDeInventario al contexto

--- Parcial1_JonathanMaria/DAL/Contexto.cs
+++ Parcial1_JonathanMaria/DAL/Contexto.cs
@@ -1,16 +1,13 @@
 ﻿using Parcial1_JonathanMaria.Entidades;
-using System;
-using System.Collections.Generic;
+using ProyectoParcial1.Entidades;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Parcial1_JonathanMaria.DAL
 {
     public class Contexto : DbContext
     {
         public DbSet<Productos> Productos { get; set; }
+        public DbSet<ValorTotalDeInventario> ValorTotalDeInventario { get; set; }
         public Contexto() : base("ConStr")
         {   }
     }
