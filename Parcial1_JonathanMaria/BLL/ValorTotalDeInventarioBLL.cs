8d3b06d3932fe1f6abd6c572e31fd61a3f2ad3b6
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-31 20:40:16 -0400

    Agregando BLL de ValorTotalDeInventario

---
+++ Parcial1_JonathanMaria/BLL/ValorTotalDeInventarioBLL.cs
@@ -0,0 +1,76 @@
+﻿using Parcial1_JonathanMaria.DAL;
+using ProyectoParcial1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParcial1.BLL
+{
+    class ValorTotalDeInventarioBLL
+    {
+        public static bool Guardar(ValorTotalDeInventario valortotaldeinventario)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.ValorTotalDeInventario.Add(valortotaldeinventario) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(ValorTotalDeInventario valortotaldeinventario)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                db.Entry(valortotaldeinventario).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var eliminar = db.ValorTotalDeInventario.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+    }
+}
