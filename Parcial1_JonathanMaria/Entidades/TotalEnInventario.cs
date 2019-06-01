7fac041c33f6cb857e640e2908822bd15eecf1fb
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-31 20:19:40 -0400

    Agregando la entidad ValorTotalDeInventario

---
+++ Parcial1_JonathanMaria/Entidades/TotalEnInventario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParcial1.Entidades
+{
+    public class ValorTotalDeInventario
+    {
+        [Key]
+        int Id;
+        float VValorTotalDeInventario;
+
+        ValorTotalDeInventario()
+        {
+            Id = 0;
+            VValorTotalDeInventario = 0;
+        }
+
+    }
+}
