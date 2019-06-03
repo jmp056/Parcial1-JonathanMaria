2fc0006059753259d5e3961bd66fdb81f6ca696b
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-03 18:20:52 -0400

    Agregando la entidad Ubicaciones

---
+++ Parcial1_JonathanMaria/Entidades/Ubicaciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParcial1.Entidades
+{
+    public class Ubicaciones
+    {
+        [Key]
+        public int IdUbicacion { get; set; }
+        public string Ubicacion { get; set; }
+
+        public Ubicaciones()
+        {
+            IdUbicacion = 0;
+            Ubicacion = string.Empty;
+        }
+    }
+}
